9521b3ee3137e3f30e478c86572cbe44e14c2ab7
aquevedo-11 <aquevedo@umd.edu>
2024-04-16 21:39:34 -0400

    Update

--- javascript.js
+++ javascript.js
@@ -2,21 +2,14 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+        labels: ['America', 'East Asia & Pacific', 'Europe & Central Asia', 'Middle East & North Africa', 'South Asia', 'Sub-Saharan Africa'],
         datasets: [{
-            label: '# of Apples',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Average GDP per capita',
+            data: [18791.13, 14480.30, 20818.45, 13856.00, 2505.17, 2569.00],
             backgroundColor: [
-                '#ff0800'
+                '#058E3F'
             ]
         },
-        {
-            label: '# of Oranges',
-            data: [22, 10, 23, 6, 22, 13],
-            backgroundColor: [
-                '#FFA500'
-            ],
-        }
     ]
     },
     options: {
@@ -24,14 +17,14 @@ var myChart = new Chart(ctx, {
         plugins: {
             title: {
                 display: true,
-                text: 'Apples and Oranges by year',
+                text: 'Global GDP per capita by region',
                 font: {
                     size: 18
                 }
             },
             subtitle: {
                 display: true,
-                text: 'For all but one year, oranges outnumbered apples'
+                text: 'Europe & Central Asia and America lead the world in regional GDP per capita with over $15,000 per person.'
             }
         },
         scales: {
